812eccaa0bd22c57753e2db2b4460b82277775b5
udeshya17 <udeshyasharma17@gmail.com>
2024-07-08 00:32:03 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs'
 import './App.css';
 
 function App() {
+  const buttons = [7, 8, 9, "+", 4, 5, 6, "-", 3, 2, 1, "*", "C", 0, "=", "/"];
+
+  const [data, setData] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleClick = (event, item) => {
+    event.preventDefault(); 
+    if (item === "C") {
+      setData("");
+      setResult(null);
+    } else if (item === "=") {
+      try {
+        setResult(evaluate(data).toString());
+      } catch (error) {
+        setResult("Error");
+      }
+    } else {
+      setData((prevData) => prevData + item.toString());
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="head">
+      <h1>React Calculator</h1>
+      <form>
+        <input type="text" readOnly value={data} />
+        <p>{result}</p>
+        <div className='gridContainer'>
+          {buttons.map((item, index) => (
+            <button key={index} className="gridItem" onClick={(event) => handleClick(event, item)}>
+              {item}
+            </button>
+          ))}
+        </div>
+      </form>
     </div>
   );
 }
